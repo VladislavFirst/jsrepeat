b104d22795d8ff95bd0c3e19b6a98ea62b7acbd1
Vladislav <vladislav-gering@mail.ru>
2023-08-31 16:17:18 +0300

    base JS

---
+++ 01-base/theory.js
@@ -0,0 +1,43 @@
+// ========= Operators
+let a = 10
+let b = 5
+let c = 32
+
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// c = 4 ** 2 //16
+
+//========= Data types
+const age = 29
+const name = 'Vladislav'
+const isProgrammier = true
+console.log(typeof a )
+
+let x // undefined
+// null 
+
+// console.log(typeof (1/x))
+
+
+// ========== Priority
+const fullAge = 23
+const birthYear = 1999
+const currentYear = 2023
+
+// > < >= <=
+const isFullAge = currentYear - birthYear <= fullAge
+
+console.log(isFullAge)
+
+const num1 = 42
+const num2 = '42'
+
+console.log(num1 === num2)
