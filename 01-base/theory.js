f48792a812eaaef237fe188d7c9d455dcd529edf
Vladislav <vladislav-gering@mail.ru>
2023-09-07 12:30:49 +0300

    massive and objects

--- 01-base/theory.js
+++ 01-base/theory.js
@@ -1,7 +1,7 @@
-// ========= Operators
-let a = 10
-let b = 5
-let c = 32
+/*// ========= Operators
+// let a = 10
+// let b = 5
+// let c = 32
 
 // c = c + a
 // c = c - a
@@ -41,3 +41,76 @@ const num1 = 42
 const num2 = '42'
 
 console.log(num1 === num2)
+*/
+
+// const a = 100;
+// a = 20;
+// console.log(a);
+
+// console.log(a)
+// let a = 15;
+// console.log(a);
+
+// const productName = 'Beer'
+// const productPrice = 400
+// let productQuantity = 4
+// let resultCost = productPrice * productQuantity
+// // console.log(productName )
+// console.log('Вы купили ' + productName + "(" + productQuantity + "шт.)" + "по" + productPrice + "₽. Всего было потрачено:" + resultCost + "₽")
+
+//4
+/*console.log(typeof 'Hello')
+console.log(typeof 10)
+console.log(typeof {})
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof [])
+console.log(typeof (() => {}))
+*/
+
+// console.log(10 == 10)
+// console.log(10 != 10)
+// console.log(12 == 'Привет')
+// console.log(10 < 11)
+// console.log(12 > 20)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false || true)
+// console.log(true || false)
+// console.log(true || true)
+// console.log(10 <= 10)
+// console.log(18 <= 20)
+// console.log(50 >= 50)
+// console.log(51 >= 50)
+// console.log({} === {})
+// console.log({} == {})
+
+// const a = {};
+// const b = a;
+// console.log(a === b);
+
+
+// let a = 31
+// if (a % 2 == 0 ) console.log('chet')
+// else console.log('nechet') 
+
+// //2
+// for (let a = 0; a <= 1000; a++) {
+//     if ((a % 2 == 1) || (a % 12 == 0))
+//     console.log(a)
+// }
+
+let a = [1, 10, 15, -100, -23, 19, 15032]
+let b = a * 1.25
+console.log (b)
+
+
+
+
+
+
+
+
+
