720143153b6466ffb25e3d04754addc0672c5f0e
Vladislav <vladislav-gering@mail.ru>
2023-09-19 13:26:43 +0300

    Num, string, date

--- array-theory.js
+++ array-theory.js
@@ -1,4 +1,4 @@
-const names = ['Владислав', 'Мария', 'Елена', 'Ксения']
+// const names = ['Владислав', 'Мария', 'Елена', 'Ксения']
 
 // names.push('Алена')  //fast 
 // names.unshift('Алена')
@@ -104,8 +104,107 @@ const people = [
 //     .reduce((acc, p) => acc + p, 0)
 // console.log(sumBudget)
 
-const string = 'Привет, как дела?'
+// const string = 'Привет, как дела?'
 
-const reversed = string.split('').toReversed().join('!').split('').filter((c) => c != '!').join()
+// const reversed = string.split('').toReversed().join('!').split('').filter((c) => c != '!').join()
 
-console.log(reversed)
+// console.log(reversed)
+//Homework
+//1
+/*
+const john = {
+    name: 'Vladislav',
+    age: 23,
+    pet: 'Murka'
+}
+
+const ann = {
+    name: 'Elena',
+    age: 23,
+    pet: 'Tiger'
+}
+
+const out = function (ob){
+    return console.log('Меня зовут ' + ob.name + ', мне ' + ob.age + ', у меня есть питомец ' + ob.pet)
+}
+
+out(john)
+out(ann)
+
+const incrementAge = function(ob) {
+    ob.age +=1
+    return 
+}
+
+incrementAge(john)
+out(john)
+
+for (let key in john){
+    console.log(key, john[key])
+}
+*/
+//2
+/*
+function sum(array) {
+    let summary = 0
+    for(let i = 0; i < array.length ; i++){
+        summary += array[i]
+        
+    }
+    return summary
+  }
+  
+  const arr = [1, 50, 20, 75, 90];
+  
+  console.log(sum(arr)); // 236
+*/
+
+//3
+/*Задание #3
+Создайте пустой массив names.
+Добавьте в него 5 любых имен.
+Выведите массив в консоль.
+Уберите из начала и конца массива по 1 элементу.
+Снова выведите массив в консоль.*/
+/*
+const names = []
+
+names.push('Maks', 'VLad', 'Elena', 'Igor', 'Gleb')
+
+console.log(names)
+
+names.shift()
+console.log(names)
+names.pop()
+console.log(names)
+*/
+
+
+//4
+/*
+Дан массив names. Необходимо сделать функцию, которая на основе этого массива генерирует строку c HTML. Она представляет из себя список элементов (<ul>), где каждый элемент списка (<li>) содержит элемент массива.
+*/
+/*
+const names = ['Bob', 'Klar', 'Petya', 'Ilya', 'Viktoriya'];
+const namesList = document.querySelector('.names_list')
+namesList.innerHTML = ""
+names.forEach((item, i) => {
+    namesList.innerHTML +=`
+    <li class="name_item">${i + 1} ${item}</li>`
+})
+*/
+
+
+//5 
+//Необходимо выполнить деструктуризацию объекта (для всех полей). Выведите полученные переменные в консоль. 
+const obj = {
+	age: 22,
+	favColor: 'red',
+	height: 188,
+	pet: 'dog',
+	money: 12300
+};
+
+const { age: age, favColor: favColor, height: height, pet: pet, money: money} = obj
+
+console.log(age, favColor, height, pet, money)
